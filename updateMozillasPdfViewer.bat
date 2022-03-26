7e01d413b880f6c35a5e32e4199ada0c190635e5
itechbear <itechbear@gmail.com>
2022-03-26 17:58:15 +0800

    update

--- updateMozillasPdfViewer.bat
+++ updateMozillasPdfViewer.bat
@@ -1,17 +1,23 @@
-cd ../mypdf.js
-gulp minified
-gulp minified-es5
+cd ..\itechbear-pdf.js
+start /wait npm install gulp-cli
+start /wait npm install
+git checkout 2.13
+start /wait .\node_modules\.bin\gulp minified
+start /wait .\node_modules\.bin\gulp minified-legacy
 
-cd ../ngx-extended-pdf-viewer
+cd ..\itechbear-ngx-extended-pdf-viewer
+git checkout 12.0.1
+start /wait npm install
 
-xcopy /S/E ../mypdf.js/build/minified/web/locale ./projects/ngx-extended-pdf-viewer/src/assets/locale/
-xcopy /S/E ../mypdf.js/build/minified/web/images/*.svg ./projects/ngx-extended-pdf-viewer/src/assets/imges/
-xcopy /S/E ../mypdf.js/build/minified/web/cmaps ./projects/ngx-extended-pdf-viewer/src/assets/cmaps/
-xcopy ../mypdf.js/build/minified/build/pdf.* ./projects/ngx-extended-pdf-viewer/src/assets/
-xcopy ../mypdf.js/build/minified/web/viewer.js* ./projects/ngx-extended-pdf-viewer/src/assets/
-xcopy ../mypdf.js/build/minified/web/viewer.min* ./projects/ngx-extended-pdf-viewer/src/assets/
-xcopy ../mypdf.js/build/minified-es5/build/pdf* ./projects/ngx-extended-pdf-viewer/src/assets/
-xcopy ../mypdf.js/build/minified-es5/web/viewer-* ./projects/ngx-extended-pdf-viewer/src/assets/
+SET PATH=%PATH%;C:\Windows\WinSxS\amd64_microsoft-windows-xcopy_31bf3856ad364e35_10.0.19041.1_none_18e6b82c93a9c5f6
+xcopy ..\itechbear-pdf.js\build\minified\web\locale .\projects\ngx-extended-pdf-viewer\assets\locale\ /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified\web\images\*.svg .\projects\ngx-extended-pdf-viewer\assets\imges\  /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified\web\cmaps .\projects\ngx-extended-pdf-viewer\assets\cmaps\ /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified\build\pdf.* .\projects\ngx-extended-pdf-viewer\assets\ /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified\web\viewer.js* .\projects\ngx-extended-pdf-viewer\assets\ /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified\web\viewer.min* .\projects\ngx-extended-pdf-viewer\assets\ /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified-legacy\build\pdf* .\projects\ngx-extended-pdf-viewer\assets\ /S/E/Y
+xcopy ..\itechbear-pdf.js\build\minified-legacy\web\viewer-* .\projects\ngx-extended-pdf-viewer\assets\ /S/E/Y
 
 cd addBaseLanguages
 node index.js assets
@@ -25,3 +31,9 @@ cd add-version-number-to-file-name
 node run-outside-ngzone.js
 node add-version-number.js
 cd ..
+
+start /wait npm run win-package
+
+cd dist\ngx-extended-pdf-viewer
+rmdir /s /q bleeding-edge
+start npm publish
